3011ddc8492833ecfe6c1e2aafcd5891bf571c99
Cody C <codyicastro@gmail.com>
2020-06-27 14:37:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,42 @@
 def create_an_empty_array
-  
+  this_is_empty = []
 end
-
+# /////////////////////////////////////////////////////
 def create_an_array
-  
+  four_elements = [4, 8, 15, 16]
 end
-
+# /////////////////////////////////////////////////////
 def add_element_to_end_of_array(array, element)
-  
+    array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+    array.concact(["arrays!"])
 end
+# /////////////////////////////////////////////////////
 
 def add_element_to_start_of_array(array, element)
   
 end
-
+# /////////////////////////////////////////////////////
 def remove_element_from_end_of_array(array)
   
 end
-
+# /////////////////////////////////////////////////////
 def remove_element_from_start_of_array(array)
   
 end
-
+# /////////////////////////////////////////////////////
 def retrieve_element_from_index(array, index_number)
   
 end
-
+# /////////////////////////////////////////////////////
 def retrieve_first_element_from_array(array)
   
 end
-
+# /////////////////////////////////////////////////////
 def retrieve_last_element_from_array(array)
   
 end
-
+# /////////////////////////////////////////////////////
 def update_element_from_index(array, index_number, element)
 
 end
